6b3f149b1ce307e2a657f7be9e175297474a943d
HenriqueBechis <125585444+HenriqueBechis@users.noreply.github.com>
2025-10-19 17:43:27 -0300

    Refactor AWS provider and resource definitions

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  # CORREÇÃO: Adicionado o sinal de =
   region = var.aws_region
 }
 
@@ -22,19 +21,18 @@ resource "aws_s3_bucket_versioning" "versioning" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1) [cite: 155]
-  instance_type = "t2.micro" [cite: 156]
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  # [cite_start]CORREÇÃO: Removida a citação [cite: 156] da linha abaixo
+  instance_type = "t2.micro"
   tags = {
-    Name = "Sprint5-EC2" [cite: 158]
+    Name = "Sprint5-EC2"
   }
 }
 
 output "bucket_name" {
-  # CORREÇÃO: Adicionado o sinal de =
   value = aws_s3_bucket.lab.bucket
 }
 
 output "ec2_public_ip" {
-  # CORREÇÃO: Adicionado o sinal de =
   value = aws_instance.app_server.public_ip
 }
