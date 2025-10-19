298afc4dd3a10d89ea929fa5284c82ca5cf5984d
HenriqueBechis <125585444+HenriqueBechis@users.noreply.github.com>
2025-10-19 20:12:43 -0300

    Refactor AWS variables with new naming and descriptions
    Renamed variables for AWS region and bucket name to follow naming conventions and added descriptions.

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,9 @@
-variable "awsregion" {
-  default = "us-east-1"
+variable "aws_region" {
+  description = "Região da AWS para os recursos"
+  default     = "us-east-1"
 }
 
-variable "bucketname" {
-  default = "lab-sprint5-arqnuvem"
+variable "bucket_name" {
+  description = "Nome do bucket S3. Deve ser único globalmente."
+  default     = "lab-sprint5-arqnuvem-${random_string.suffix.result}"
 }
